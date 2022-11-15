0a6fc323932da0706d2da19e026fe70768c9bdda
pitokai <crisvitoc@gmail.com>
2022-11-15 18:48:21 +0800

    refactor : refactor validation delete

--- resources/views/admin/voters/index.blade.php
+++ resources/views/admin/voters/index.blade.php
@@ -51,7 +51,7 @@
                                                 @csrf
                                                 <button type="submit"
                                                     class="inline-flex items-center px-4 py-2 bg-red-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-700 active:bg-red-900 focus:outline-none focus:border-red-900 focus:ring ring-red-300 disabled:opacity-25 transition ease-in-out duration-150"
-                                                    onclick="return confirm('Yakin menghapus Data?')">
+                                                    onclick="return confirm('Yakin menghapus Data {{ $voter->name }}')">
                                                     Delete
                                                 </button>
                                             </form>
